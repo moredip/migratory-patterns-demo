097d67ea43f4e5fa9cd45788bcdbab04358b8a21
Pete Hodgson <git@thepete.net>
2024-10-23 16:38:11 -0700

    setup HC o11y

--- latency.py
+++ latency.py
@@ -1,7 +1,20 @@
 import asyncio
+from opentelemetry import trace
 import random
+
+tracer = trace.get_tracer(__name__)
 
 
 async def simulate_latency(*, mean: float, stddev: float):
     latency = random.normalvariate(mean, stddev)
-    await asyncio.sleep(latency)
+
+    with tracer.start_as_current_span("simulate_latency") as span:
+        span.set_attributes(
+            {
+                "latency.simulated_secs": latency,
+                "latency.mean_secs": mean,
+                "latency.stddev": stddev,
+            }
+        )
+
+        await asyncio.sleep(latency)
--- otel.py
+++ otel.py
@@ -1,21 +1,42 @@
+import os
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor, BatchSpanProcessor
+
+from settings import Settings
+
+settings = Settings()
+
+HONEYCOMB_DATASET = "canary-launch-demo"
+HONEYCOMB_HEADERS = {
+    "x-honeycomb-team": settings.honeycomb_api_key,
+    "x-honeycomb-dataset": HONEYCOMB_DATASET,
+}
+HONEYCOMB_ENDPOINT = "https://api.honeycomb.io/v1/traces"
 
 # Resource can be required for some backends, e.g. Jaeger
 # If resource wouldn't be set - traces wouldn't appears in Jaeger
 resource = Resource(attributes={"service.name": "demo-service"})
 
-trace.set_tracer_provider(TracerProvider(resource=resource))
+trace_provider = TracerProvider(resource=resource)
 
-otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+jaeger_processor = SimpleSpanProcessor(
+    OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+)
+trace_provider.add_span_processor(jaeger_processor)
 
-span_processor = SimpleSpanProcessor(otlp_exporter)
+honeycomb_processor = BatchSpanProcessor(
+    OTLPSpanExporter(
+        endpoint=HONEYCOMB_ENDPOINT,
+        headers=HONEYCOMB_HEADERS,
+    )
+)
+trace_provider.add_span_processor(honeycomb_processor)
 
-trace.get_tracer_provider().add_span_processor(span_processor)
+trace.set_tracer_provider(trace_provider)
 
 
 def instrument_app(app):
